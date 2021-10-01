582798dd6e7b75767d1d98550de6d61c9817fefc
batflarrow <dwivedia2001@gmail.com>
2021-10-01 14:18:30 +0530

    refactor model files

--- models/question.js
+++ models/question.js
@@ -1,21 +1,15 @@
-//import the MongoDB module 
 let mongoose = require('mongoose');
 let Schema = mongoose.Schema;
-let jwt = require('jsonwebtoken');
-//import dotenv module
-require('dotenv').config();
 
-//connect the cluster to the schema
-mongoose.connect(process.env.db, { useNewUrlParser: true, useUnifiedTopology: true });
 
 let Question = new Schema({
-    text: {type:String},
-    image: {type:String},
-    id:{type:Number, required:true, unique:true},
-    points: {type:Number, required: true},
-    answer: {type:String, required:true},
-    link: {type: String}
-})
+    text: { type: String },
+    image: { type: String },
+    id: { type: Number, required: true, unique: true },
+    points: { type: Number, required: true },
+    answer: { type: String, required: true },
+    link: { type: String }
+});
 
 
 
--- models/user.js
+++ models/user.js
@@ -1,25 +1,17 @@
-//import the MongoDB module 
 let mongoose = require('mongoose');
 let Schema = mongoose.Schema;
-let jwt = require('jsonwebtoken');
-//import dotenv module
-require('dotenv').config();
 
-//connect the cluster to the schema
-mongoose.connect(process.env.db, { useNewUrlParser: true, useUnifiedTopology: true });
 
-/*Create a new model/object by the name of User
- *whenever a new user signs up this creates a new user object in the database*/
 let User = new Schema({
-    username: {type: String, required: true, unique: true},
-    email: {type: String, required: true, unique: true},
-    password: {type: String, required: true, index:true, sparse:true},
-    isAdmin: {type: Boolean, default: false},
+    username: { type: String, required: true, unique: true },
+    email: { type: String, required: true, unique: true },
+    password: { type: String, required: true, index: true, sparse: true },
+    isAdmin: { type: Boolean, default: false },
     levelsCompleted: [Number],
-    score: {type: Number, default:0},
-    lastSubmit:{type: Date, default: Date.now},
-    lastSuccess:{type: Date, default: Date.now}
-})
+    score: { type: Number, default: 0 },
+    lastSubmit: { type: Date, default: Date.now },
+    lastSuccess: { type: Date, default: Date.now }
+});
 
 
 
