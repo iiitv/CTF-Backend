6e13fa3a2346f344cadfbb057b645256143f1729
SwapnilSoni1i999 <swapnil.soni12345@gmail.com>
2021-10-01 18:46:32 +0530

    Add login functionality and setup bcryptjs

--- controllers/users/login.js
+++ controllers/users/login.js
@@ -1,9 +1,21 @@
-const login = async (req, res, next) => {
-    console.log('*** Came Inside Login ***');
-    try {
+const bcrypt = require('bcryptjs')
+const jwt = require('jsonwebtoken')
+const User = require('../../models/user')
 
+const login = async (req, res, next) => {
+    try {
+        const { email, username, password } = req.body
+        const user = await User.findOne({ $or: [{email}, {username}] })
+        if (!user) {
+            return res.status(404).json({ message: 'Email or password is wrong!' })
+        }
+        if (bcrypt.compareSync(password, user.password)) {
+            const token = jwt.sign({ id: user.id }, process.env.JWT_SECRET)
+            return res.status(200).json({ message: "Login successful!", token })
+        }
+        return res.status(401).json({ message: 'Email or password is wrong!' })
     } catch (error) {
-        console.log(error);
+        return res.status(500).json({ message: 'Internal server error!' })
     }
 };
 
