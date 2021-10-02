7e0672a6d172aafbcdf1a27063c8fe0d64b8f112
Joel Lau <29514264+JoelLau@users.noreply.github.com>
2021-10-02 10:10:11 +0800

    updated mongoose connection config (see https://mongoosejs.com/docs/migrating_to_6.html#no-more-deprecation-warning-options)

--- config/database.js
+++ config/database.js
@@ -1,16 +1,20 @@
 const mongoose = require('mongoose');
+const dotenv = require('dotenv');
+dotenv.config();
 
 mongoose.Promise = global.Promise;
 const connectDB = async () => {
-    try {
-        await mongoose.connect(process.env.DATABASE_URL);
+  try {
+    await mongoose.connect(process.env.DATABASE_URL, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
 
-        console.log("MongoDB connected...");
-    } catch (err) {
-        console.log(err.message);
-        process.exit(1);
-    }
+    console.log('MongoDB connected...');
+  } catch (err) {
+    console.log(err.message);
+    process.exit(1);
+  }
 };
 
 module.exports = connectDB;
-
