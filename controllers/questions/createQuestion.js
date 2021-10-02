4e981329e0e876696507bc95003f0ccbf74b39c2
Joel Lau <29514264+JoelLau@users.noreply.github.com>
2021-10-02 13:51:34 +0800

    completed createQuestion

--- controllers/questions/createQuestion.js
+++ controllers/questions/createQuestion.js
@@ -1,5 +1,16 @@
+const Question = require('../../models/question');
+
 const createQuestion = async (req, res, next) => {
-  return res.status(500).json({ message: 'Under contruction!' });
+  try {
+    const newQuestion = new Question(req.body);
+    const question = await newQuestion.save();
+
+    const { id: questionId } = question;
+    res.set('Location', `/questions/${questionId}`);
+    return res.status(201).json(question);
+  } catch (error) {
+    return res.status(500).json(error);
+  }
 };
 
 module.exports = createQuestion;
--- controllers/questions/readQuestions.js
+++ controllers/questions/readQuestions.js
@@ -1,8 +1,8 @@
-const questionModel = require('../../models/question');
+const Question = require('../../models/question');
 
 const readQuestions = async (req, res, next) => {
   try {
-    const questions = await questionModel.find({});
+    const questions = await Question.find({});
     return res.status(200).json(questions);
   } catch (error) {
     return res.status(500).json(error);
